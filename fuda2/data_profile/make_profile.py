24fb8f7271be477416481708d9a00bcd14f81fad
xianglishan <91013631+xianglishan@users.noreply.github.com>
2024-01-27 15:15:22 +0000

    a

--- fuda2/data_profile/make_profile.py
+++ fuda2/data_profile/make_profile.py
@@ -2,6 +2,8 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 data = pd.read_csv("s3://xianglishan-sandbox/signate-fuda2/train.csv")
-    profile = ProfileReport(data, title="Profiling Report")
+profile = ProfileReport(data, title="Profiling Report")
 # profile.to_notebook_iframe()
 profile.to_file("./train_data_report.html")
+
+print(  'asdf'    )    
