86fdbc90567760769aa9d13acae1a46d598bbc70
xianglishan <91013631+xianglishan@users.noreply.github.com>
2024-01-27 15:20:23 +0000

    profiling train data

--- fuda2/data_profile/make_profile.py
+++ fuda2/data_profile/make_profile.py
@@ -4,8 +4,4 @@ from ydata_profiling import ProfileReport
 data = pd.read_csv("s3://xianglishan-sandbox/signate-fuda2/train.csv")
 profile = ProfileReport(data, title="Profiling Report")
 # profile.to_notebook_iframe()
-
-
 profile.to_file("./train_data_report.html")
-
-str = "hoge" "fuga" "piyo"
